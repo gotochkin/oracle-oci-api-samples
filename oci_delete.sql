b6c3b464ef962f7e494842c44bc34781ce197543
otochkin <glebot@hotmail.com>
2022-11-28 08:34:55 -0500

    more sql scripts

--- oci_delete.sql
+++ oci_delete.sql
@@ -1,14 +1,34 @@
--- #########################
--- ## Terminate instance  ##
--- #########################
+-- ############################################################################
+-- Copyright 2022 Gleb Otochkin
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http:--www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ############################################################################
+-- Terminate an instance
+-- ############################################################################
 set serveroutput on
 DECLARE
     response         dbms_cloud_oci_cr_compute_terminate_instance_response_t;
     instance_id      varchar2(200);
     json_obj         json_object_t;
     l_keys           json_key_list;
+    instance_id     varchar2(100);
+    credential_name  varchar2(100);
 BEGIN
-    instance_id := instance_id;
+    -- Instance ID to be deleted
+    instance_id:='&credential_name';
+    -- Credential name for the OCI user credentials
+    credential_name:='&credential_name';
+    -- Run the function and get response 
     response := dbms_cloud_oci_cr_compute.terminate_instance(instance_id => instance_id, preserve_boot_volume => 0, region => 'ca-toronto-1', credential_name => credential_name);
     
     -- Response Headers
